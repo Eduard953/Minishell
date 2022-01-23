f7be9747b79e79f162391af2b030443cb0c242cd
GQDeltex <operitum@gmail.com>
2022-01-23 15:48:42 +0100

    Fix major Memory allocation issue

--- export.c
+++ export.c
@@ -6,7 +6,7 @@
 /*   By: ebeiline <ebeiline@42wolfsburg.de>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/23 15:26:40 by ebeiline          #+#    #+#             */
-/*   Updated: 2022/01/23 17:37:48 by ebeiline         ###   ########.fr       */
+/*   Updated: 2022/01/23 15:46:00 by pstengl          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,19 +17,18 @@ char **builtin_export(char **arg, char **envp)
 	char **temp;
 	char **tokens;
 	char *value;
-	
-	int argslen;
+	int envplen;
 	int i;
-	
-	i = 0;
-	argslen = 0;
-	
+
+
 	tokens = ft_split(arg[1], '=');
 	value = ft_in_envp(envp, tokens[0]);
 	ft_print("MAybe here?\n");
-	while (arg[argslen] != NULL)
-		argslen++;
-	temp = ft_calloc(sizeof(char *), (argslen + 2));
+	envplen = 0;
+	while (envp[envplen] != NULL)
+		envplen++;
+	temp = ft_calloc(sizeof(char *), (envplen + 2));
+	i = 0;
 	while (envp[i] != NULL)
 	{
 		if (!(ft_strncmp(envp[i], arg[1], ft_strlen(tokens[0]) + 1)))
@@ -49,19 +48,17 @@ char **builtin_export(char **arg, char **envp)
 char **builtin_unset(char **arg, char **envp)
 {
 	char **temp;
-	char *value;
-	
-	int argslen;
+
+	int envplen;
 	int i;
 	int j;
-	
+
 	i = 0;
 	j = 0;
-	argslen = 0;
-	value = ft_in_envp(envp, arg[1]);
-	while (arg[argslen] != NULL)
-		argslen++;
-	temp = ft_calloc(sizeof(char *), (argslen + 2));	
+	envplen = 0;
+	while (envp[envplen] != NULL)
+		envplen++;
+	temp = ft_calloc(sizeof(char *), (envplen + 2));
 	while (envp[i] != NULL)
 	{
 		if (!(ft_strncmp(envp[i], arg[1], ft_strlen(arg[1]))))
