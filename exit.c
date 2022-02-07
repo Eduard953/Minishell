d45a00be2244df1a57a2007a2ac15fbc51bf79d3
Eduard Beiline <eduardbeiline@Eduards-Air.local>
2022-02-07 15:39:58 +0100

    added multiple args to export/unset

--- exit.c
+++ exit.c
@@ -6,13 +6,27 @@
 /*   By: ebeiline <ebeiline@42wolfsburg.de>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/23 15:21:22 by ebeiline          #+#    #+#             */
-/*   Updated: 2022/02/06 15:35:17 by ebeiline         ###   ########.fr       */
+/*   Updated: 2022/02/07 14:38:08 by ebeiline         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	builtin_exit(void)
+int	ft_isnumber(char *num)
 {
-	exit(0);
+	int	i;
+
+	i = -1;
+	while (num[++i])
+		if (!(ft_isdigit(num[i])))
+			return (0);
+	return (1);
+}
+
+int	builtin_exit(char **args)
+{
+	if (ft_arrlen(args) > 1)
+		if (ft_isnumber(args[1]))
+			exit(ft_atoi(args[1]));
+	exit (0);
 }
--- export.c
+++ export.c
@@ -6,69 +6,68 @@
 /*   By: ebeiline <ebeiline@42wolfsburg.de>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/23 15:26:40 by ebeiline          #+#    #+#             */
-/*   Updated: 2022/02/07 14:09:12 by ebeiline         ###   ########.fr       */
+/*   Updated: 2022/02/07 15:38:09 by ebeiline         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	builtin_export(char **arg, char ***envp)
+int	ft_checkname(char *name)
+{
+	if (ft_isdigit(name[0]) || (ft_strlen(name) < 1))
+		return (0);
+	while (*name)
+	{
+		if (!(ft_isalnum(*name) || (*name == '_')))
+			return (0);
+		name++;
+	}
+	return (1);
+}
+
+int	export_helper(char ***envp, char *token, char *value, char *arg)
 {
 	char	**temp;
-	char	**tokens;
-	char	*value;
 	int		i;
 
-	if (!(ft_strchr(arg[1], '=')))
-		return (0);
-	tokens = ft_split(arg[1], '=');
-	value = ft_in_envp(*envp, tokens[0]);
 	if (value)
 	{
 		i = 0;
 		while ((*envp)[i] != NULL)
 		{
-			if (ft_strncmp((*envp)[i], arg[1], ft_strlen(tokens[0]) + 1) == 0)
+			if (ft_strncmp((*envp)[i], arg, ft_strlen(token) + 1) == 0)
 			{
 				free((*envp)[i]);
-				(*envp)[i] = ft_strdup(arg[1]);
+				(*envp)[i] = ft_strdup(arg);
 			}
 			i++;
 		}
-		free(value);
-		ft_arrclear(tokens, free);
 		return (0);
 	}
 	temp = ft_calloc(ft_arrlen(*envp) + 2, sizeof(char *));
 	if (!temp)
 		return (error(5));
 	temp = ft_memcpy(temp, *envp, sizeof(char *) * ft_arrlen(*envp));
-	temp[ft_arrlen(temp)] = ft_strdup(arg[1]);
-	free(value);
+	temp[ft_arrlen(temp)] = ft_strdup(arg);
 	free(*envp);
-	ft_arrclear(tokens, free);
 	*envp = temp;
 	return (0);
 }
 
-char	**builtin_unset(char **arg, char **envp)
+char	**unset_helper(char **envp, char *arg)
 {
 	char	**temp;
-	int		envplen;
-	int		i;
 	int		j;
+	int		i;
 
 	i = 0;
 	j = 0;
-	envplen = 0;
-	while (envp[envplen] != NULL)
-		envplen++;
-	temp = ft_calloc(sizeof(char *), (envplen + 2));
+	temp = ft_calloc(sizeof(char *), (ft_arrlen(envp) + 2));
 	while (envp[i] != NULL)
 	{
-		if (!(ft_strncmp(envp[i], arg[1], ft_strlen(arg[1]))))
+		if (!(ft_strncmp(envp[i], arg, ft_strlen(arg))))
 		{
-			if (envp[i][ft_strlen(arg[1])] == '=')
+			if (envp[i][ft_strlen(arg)] == '=')
 				i++;
 		}
 		if (envp[i] == NULL)
@@ -79,4 +78,43 @@ char	**builtin_unset(char **arg, char **envp)
 	}
 	free(envp);
 	return (temp);
+}
+
+int	builtin_export(char **arg, char ***envp)
+{
+	int		j;
+	int		returncode;
+	char	**tokens;
+	char	*value;
+
+	j = 0;
+	returncode = 0;
+	while (arg[++j])
+	{
+		if (!(ft_strchr(arg[j], '=')))
+		{
+			if (!(ft_checkname(arg[j])))
+				error (1);
+			continue ;
+		}
+		tokens = ft_split(arg[j], '=');
+		value = ft_in_envp(*envp, tokens[0]);
+		if (ft_checkname(tokens[0]))
+			returncode += export_helper(envp, tokens[0], value, arg[j]);
+		else
+			error (1);
+		free(value);
+		ft_arrclear(tokens, free);
+	}
+	return ((returncode > 0));
+}
+
+char	**builtin_unset(char **arg, char **envp)
+{
+	int		j;
+
+	j = 0;
+	while (arg[++j])
+		envp = unset_helper(envp, arg[j]);
+	return (envp);
 }
