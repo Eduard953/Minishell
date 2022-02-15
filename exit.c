64032aee0363369c54e423aea93ba89b0372158a
Eduard Beiline <eduardbeiline@Eduards-MacBook-Air.local>
2022-02-15 14:36:25 +0100

    minor changes

--- exit.c
+++ exit.c
@@ -6,7 +6,7 @@
 /*   By: ebeiline <ebeiline@42wolfsburg.de>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/23 15:21:22 by ebeiline          #+#    #+#             */
-/*   Updated: 2022/02/14 15:27:25 by ebeiline         ###   ########.fr       */
+/*   Updated: 2022/02/14 16:06:49 by ebeiline         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,17 +25,17 @@ int	ft_isnumber(char *num)
 	return (1);
 }
 
-int check_arg(char **args)
+int	check_arg(char **args)
 {
 	int	i;
 
 	i = 1;
 	if (ft_arrlen(args) > 2)
-		return(error(1)); // need the right errorcodes
+		return (error(1)); // need the right errorcodes
 	while (args[i])
 	{
 		if (!ft_isnumber(args[i]))
-			return(error(127)); // need the right errorcodes
+			return (error(127)); // need the right errorcodes
 		i++;
 	}
 	return (0);
@@ -46,10 +46,7 @@ int	builtin_exit(char **args, int returncode)
 	if (check_arg(args))
 		return (check_arg(args));
 	if (ft_arrlen(args) == 2)
-	{
 		returncode = ft_atoi(args[1]) % 256;
-		printf("here %d\n", returncode);	
-	}
 	//clean(); // Free all memory?
 	exit(returncode);
 }
