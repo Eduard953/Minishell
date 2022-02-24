20d5dddd09cd86b416836b6a753c7057e7c5f636
GQDeltex <operitum@gmail.com>
2022-02-24 17:30:34 +0100

    Fix upper part of execute_commands.c

--- execute_commands.c
+++ execute_commands.c
@@ -6,17 +6,53 @@
 /*   By: ebeiline <ebeiline@42wolfsburg.de>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/23 15:18:36 by ebeiline          #+#    #+#             */
-/*   Updated: 2022/02/24 15:10:04 by pstengl          ###   ########.fr       */
+/*   Updated: 2022/02/24 17:29:33 by pstengl          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	launch_exe(char **arg, char ***envp, int returncode, int wait_finish)
+int	replace_arg_with_path(char ***arg, char **envp)
 {
-	int		pid;
 	char	*path;
 
+	if (!ft_isalpha((*arg)[0][0]))
+		return (0);
+	path = find_in_path((*arg)[0], envp);
+	if (path == NULL)
+	{
+		ft_putstr_fd("Command not Found\n", 2);
+		return (1);
+	}
+	free(arg[0]);
+	(*arg)[0] = path;
+	return (0);
+}
+
+int	run_external(char **arg, char **envp, int wait_finish)
+{
+	int		pid;
+	int		returncode;
+
+	if (!replace_arg_with_path(&arg, envp))
+		return (127);
+	pid = fork();
+	if (!pid)
+	{
+		execve(arg[0], arg, envp);
+		perror("execve");
+		exit(1);
+	}
+	if (!wait_finish)
+		return (0);
+	waitpid(pid, &returncode, 0);
+	if (WIFEXITED(returncode))
+		return (WEXITSTATUS(returncode));
+	return (0);
+}
+
+int	launch_exe(char **arg, char ***envp, int returncode, int wait_finish)
+{
 	if (ft_strcmp(arg[0], "echo") == 0)
 		return (builtin_echo(arg));
 	if (ft_strcmp(arg[0], "cd") == 0)
@@ -34,32 +70,7 @@ int	launch_exe(char **arg, char ***envp, int returncode, int wait_finish)
 	}
 	if (ft_strcmp(arg[0], "pwd") == 0)
 		return (builtint_pwd());
-	if (ft_isalpha(arg[0][0]))
-	{
-		path = find_in_path(arg[0], (*envp));
-		if (path == NULL)
-		{
-			ft_putstr_fd("Command not Found\n", 2);
-			return (127);
-		}
-		free(arg[0]);
-		arg[0] = path;
-	}
-	pid = fork();
-	if (!pid)
-	{
-		execve(arg[0], arg, (*envp));
-		perror("execve");
-		exit(1);
-	}
-	returncode = 0;
-	if (wait_finish)
-	{
-		waitpid(pid, &returncode, 0);
-		if (WIFEXITED(returncode))
-			returncode = WEXITSTATUS(returncode);
-	}
-	return (returncode);
+	return (run_external(arg, *envp, wait_finish));
 }
 
 int	execute_command(t_list *commands, char ***envp, int returncode)
